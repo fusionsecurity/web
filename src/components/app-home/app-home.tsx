819694242eb24d53d7c99bdb5c3567c84b10287e
gglass <greg.glass@fusionsecurity.co>
2018-12-04 09:17:55 -0600

    initial commit

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -7,23 +7,16 @@ import { Component } from '@stencil/core';
 export class AppHome {
 
   render() {
-    return [
-      <ion-header>
-        <ion-toolbar color="primary">
-          <ion-title>Home</ion-title>
-        </ion-toolbar>
-      </ion-header>,
-
-      <ion-content padding>
-        <p>
-          Welcome to the PWA Toolkit. You can use this starter to build entire
-          apps with web components using Stencil and ionic/core! Check out the
-          README for everything that comes in this starter out of the box and
-          check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
-        </p>
-
-        <ion-button href="/profile/ionic" expand="block">Profile page</ion-button>
-      </ion-content>
-    ];
+    return (
+            <section class="bg-primary text-centered text-white" id="main">
+                <div class="center">
+                    <h1>Fusion Security</h1>
+                    <h3>coming soon</h3>
+                    <p class="has-top-margin">Contact Connor Borchgrevink at connor.borkgrevink@fusionsecurity.co to
+                        learn more.</p>
+                </div>
+            </section>
+        )
+    ;
   }
 }
